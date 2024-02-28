3346a31e7409a1c3462f4839a7d29efc231d3fe7
Gopala <gopalapatel1234@gmail.com>
2024-02-28 22:29:55 +0530

    Added animated background

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Bg from "./components/Bg";
 import "./App.css";
 function App() {
   const [name, setName] = useState("");
@@ -12,21 +13,19 @@ function App() {
   }, [transactions]);
   const getAmount = (str) => {
     let amount = 0;
-    for (let i = 1; i < str.length; i++) 
-      amount = amount * 10 + (str[i] - '0');
-    if(str[0] == '-')
-      return -1*amount;
+    for (let i = 1; i < str.length; i++) amount = amount * 10 + (str[i] - "0");
+    if (str[0] == "-") return -1 * amount;
     return amount;
   };
   const getTransactions = async () => {
-    const url = "https://moneytraker.onrender.com/server/transactions";
+    const url = "http://localhost:8000/server/transactions";
     const res = await fetch(url);
     const data = await res.json();
     return data;
   };
   const addNewTransaction = (e) => {
     e.preventDefault();
-    const url = "https://moneytraker-1.onrender.com/server/transaction";
+    const url = "http://localhost:8000/server/transaction";
     const amount = name.split(" ")[0];
     fetch(url, {
       method: "POST",
@@ -46,65 +45,77 @@ function App() {
       });
     });
   };
-  let totalAmount=0;
-  for(const transaction of transactions)
-  {
-    totalAmount+=getAmount(transaction.Amount);
+  let totalAmount = 0;
+  for (const transaction of transactions) {
+    totalAmount += getAmount(transaction.Amount);
   }
   return (
-    <main>
-      <h1>
-        $ {totalAmount}<span>.00</span>
-      </h1>
-      <form onSubmit={addNewTransaction}>
-        <div className="basic">
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-            placeholder="+200 Received from xyz"
-          />
-          <input
-            type="datetime-local"
-            value={datetime}
-            onChange={(e) => {
-              setDateTime(e.target.value);
-            }}
-          />
-        </div>
-        <div className="description">
-          <input
-            type="text"
-            placeholder="Description"
-            value={description}
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-          />
-        </div>
-        <button type="submit">Add New Transaction</button>
-        {/* {transactions.length} */}
-      </form>
-      <div className="transactions">
-        {transactions.length > 0 &&
-          transactions.map((transaction) => (
-            <div className="transaction" key={transaction.id}>
-              <div className="left">
-                <div className="name">{transaction.Name}</div>
-                <div className="description">{transaction.Description}</div>
-              </div>
-              <div className="right">
-                <div className={"price "+(getAmount(transaction.Amount) < 0 ? "red":"green")}>
-                  {transaction.Amount}
-                </div>
-                <div className="datetime">{transaction.Datetime}</div>
-              </div>
-            </div>
-          ))}
+    <div style={{"position":"relative"}}>
+      <div style={{"position":"absolute"}}>
+        <Bg/>
       </div>
-    </main>
+      <div style={{"paddingTop":"50px"}}>
+        <main>
+          <h1 style={{"color":(totalAmount<0) ? "red":"green" }}>
+            $ {totalAmount}
+            <span>.00</span>
+          </h1>
+          <form onSubmit={addNewTransaction}>
+            <div className="basic">
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                placeholder="+200 Received from xyz"
+              />
+              <input
+                type="datetime-local"
+                value={datetime}
+                onChange={(e) => {
+                  setDateTime(e.target.value);
+                }}
+              />
+            </div>
+            <div className="description">
+              <input
+                type="text"
+                placeholder="Description"
+                value={description}
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
+              />
+            </div>
+            <button type="submit">Add New Transaction</button>
+          </form>
+          <div className="transactions">
+            {transactions.length > 0 &&
+              transactions.map((transaction) => (
+                <div className="transaction" key={transaction.id}>
+                  <div className="left">
+                    <div className="name">{transaction.Name}</div>
+                    <div className="description">{transaction.Description}</div>
+                  </div>
+                  <div className="right">
+                    <div
+                      className={
+                        "price " +
+                        (getAmount(transaction.Amount) < 0 ? "red" : "green")
+                      }
+                    >
+                      {transaction.Amount}
+                    </div>
+                    <div className="datetime">{transaction.Datetime}</div>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </main>
+      </div>
+
+    </div>
   );
 }
 export default App;
---
+++ client/src/components/Bg.jsx
@@ -0,0 +1,48 @@
+import './Bg.css'
+const Bg = () => {
+  return (
+    <div className="body">
+      <div className="container">
+        <div className="bubbles">
+          <span style={{"--i":"11"}}></span>
+          <span style={{"--i":"23"}}></span>
+          <span style={{"--i":"15"}}></span>
+          <span style={{"--i":"23"}}></span>
+          <span style={{"--i":"34"}}></span>
+          <span style={{"--i":"10"}}></span>
+          <span style={{"--i":"56"}}></span>
+          <span style={{"--i":"23"}}></span>
+          <span style={{"--i":"19"}}></span>
+          <span style={{"--i":"18"}}></span>
+          <span style={{"--i":"17"}}></span>
+          <span style={{"--i":"24"}}></span>
+          <span style={{"--i":"25"}}></span>
+          <span style={{"--i":"27"}}></span>
+          <span style={{"--i":"28"}}></span>
+          <span style={{"--i":"29"}}></span>
+          <span style={{"--i":"31"}}></span>
+          <span style={{"--i":"33"}}></span>
+          <span style={{"--i":"34"}}></span>
+          <span style={{"--i":"34"}}></span>
+          <span style={{"--i":"28"}}></span>
+          <span style={{"--i":"25"}}></span>
+          <span style={{"--i":"27"}}></span>
+          <span style={{"--i":"43"}}></span>
+          <span style={{"--i":"47"}}></span>
+          <span style={{"--i":"28"}}></span>
+          <span style={{"--i":"29"}}></span>
+          <span style={{"--i":"30"}}></span>
+          <span style={{"--i":"31"}}></span>
+          <span style={{"--i":"32"}}></span>
+          <span style={{"--i":"57"}}></span>
+          <span style={{"--i":"34"}}></span>
+          <span style={{"--i":"27"}}></span>
+          <span style={{"--i":"10"}}></span>
+          <span style={{"--i":"19"}}></span>
+          <span style={{"--i":"23"}}></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Bg;
